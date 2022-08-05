48ad852193e5844c0b1192736a09123a1b045db0
Johnboy <curtlyvassell@protonmail.com>
2022-08-05 11:24:02 +0100

    New page to set vocab preferences before loop.

---
+++ settings.php
@@ -0,0 +1,18 @@
+<!--
+    Options for which vocab to start with: how many, where to start from
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Preferences</title>
+
+    <link href="css/table.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <form action="translate.php" method="POST">
+
+        <input type="submit">
+    </form>
+</body>
+</html>
