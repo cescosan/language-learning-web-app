8a27f27a8ca25218c70f3b2c781fe1c079ee3ddf
Johnboy <curtlyvassell@protonmail.com>
2022-08-05 11:24:41 +0100

    Small changes.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,12 @@
 from urllib import request as rq
 
 
+#on 1000mcw look for word 'number'; this is first first <tr>, first <td>
 
 def main():
     url = "https://1000mostcommonwords.com/1000-most-common-italian-words/"
     rq.urlopen(url)
+    
 
 
 
