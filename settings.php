3895a893cafd2fe66b8806bcc81f9dd55a3cbbde
Johnboy <curtlyvassell@protonmail.com>
2024-01-24 17:44:21 +0000

    Added php to settings to start a new session, and added "start" and "end" input fields to form to specify which vocab is loaded in.

--- settings.php
+++ settings.php
@@ -1,5 +1,22 @@
+
+
+<?php
+
+//starts a new session, brings in vocab and translations
+
+//intial is TRUE until the first translations are done
+
+session_start();
+
+$_SESSION["initial"] = TRUE;
+
+
+?>
+
+
 <!--
     Options for which vocab to start with: how many, where to start from
+    Also copies in vocab and translations and stores these in $_SESSION
 -->
 
 <!DOCTYPE html>
@@ -10,7 +27,20 @@
     <link href="css/table.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+    <h1>Vocab Memorisation</h1>
+
     <form action="translate.php" method="POST">
+        <input type="number" name="start" id="start">
+        <label for="start">Start</label>
+
+        <br>
+
+        <input type="number" name="end" id="end">
+        <label for="end">End</label>
+
+        <!-- todo: validation for start<end -->
+
+        <br>
 
         <input type="submit">
     </form>
